2fb400c8ed7fdf8a063dbaaf7a4a2021bc2dc38b
Jabair <jabairk@gmail.com>
2021-08-12 12:54:18 -0400

    Functions done

---
+++ intro_to_functions/javascript.js
@@ -0,0 +1,37 @@
+// Sample function
+// function Addition(num1, num2) {
+// 	var total = (num1 * 3.14) + (num2 / 8); 
+
+// 	return total;
+// }
+
+// var newTotal = Addition(10, 15);
+// var newTotal2 = Addition(57, 1057);
+
+// console.log(newTotal, newTotal2);
+
+
+function One(num1, num2){
+	return (
+		num1/num2
+	)
+}
+
+var newTotal = One(77, 7)
+console.log(newTotal)
+
+// function Addition() {
+// 	// alert('I am a function');
+// 	var num1 = 15;
+// 	var num2 = 35;
+
+// 	var total = num1 + num2;
+
+// 	return total;
+
+	
+
+// }
+
+// var newTotal = Addition();
+// alert(newTotal);
