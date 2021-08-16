7d591ac9d29306e9e926362688bf76ec298fc09c
Jabair <jabairk@gmail.com>
2021-08-16 14:44:35 -0400

    Added parseInt

--- browser_calculator/javascript.js
+++ browser_calculator/javascript.js
@@ -1,25 +1,34 @@
 // basic calculator function.
 // Doesn't return anything; it changed the #answer in the DOM.
+
+
 function Calculator() {
 	let answer = document.querySelector('#answer');
-	let num1 = Number(document.querySelector('#num1').value);
-	let num2 = Number(document.querySelector('#num2').value);
+	let num1 = (document.querySelector('#num1').value);
+	let num2 = (document.querySelector('#num2').value);
 	let select = document.querySelector('select');
 	let operator = select.options[select.selectedIndex].value;
 	let theCalculatedAnswer;
 
+	let int1 = parseInt(num1)
+	let int2 = parseInt(num2)
+	
+	console.log(int1 + int2)
+	
+	
 	if(operator == 'add') {
-		theCalculatedAnswer = num1 + num2;
+		theCalculatedAnswer = int1 + int2;
 	} else if(operator == 'minus') {
-		theCalculatedAnswer = num1 - num2;
+		theCalculatedAnswer = int1 - int2;
 	} else if(operator == 'divide') {
-		theCalculatedAnswer = num1 / num2;
+		theCalculatedAnswer = int1 / int2;
 	} else if(operator == 'multiply') {
-		theCalculatedAnswer = num1 * num2;
+		theCalculatedAnswer = int1 * int2;
 	} else {
-		// alert('Please select an operator');
 		theCalculatedAnswer = "Invalid operator";
 	}
-
+	
 	answer.innerHTML = theCalculatedAnswer;
 }
+
+// alert('Please select an operator');
