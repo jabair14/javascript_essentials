be1483426d6b59c607a9ed191af75b7f36128463
Jabair <jabairk@gmail.com>
2021-08-17 12:39:44 -0400

    anonymous_functions

---
+++ anonymous_functions/javascript.js
@@ -0,0 +1,17 @@
+// var x = function(num1, num2) {
+// 	console.log(num1 + num2);
+// }
+
+// x(1, 2);
+// x(3, 4);
+// x(5, 6);
+
+
+//anonymous functions are not global
+let j = function(num1, num2) {
+	console.log(num1 * num2);
+}
+
+j(1, 1)
+j(12, 12)
+j(9, 11)
