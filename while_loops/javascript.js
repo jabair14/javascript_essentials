3d383fc38aeca4bcc0be50bba0ce95b871e05693
Jabair <jabairk@gmail.com>
2021-09-09 14:35:55 -0400

    while loops

---
+++ while_loops/javascript.js
@@ -0,0 +1,25 @@
+// While loop example
+// This will run for as long as `i` is less than or equal to 10.
+var i = 3;
+while(i <= 10) {
+	console.log(i);
+
+	// DONT FORGET THIS
+	i++;
+}
+
+
+// Do..while loop example
+// This will only run once and realize it's condition is not met
+// And never run again. 
+var x = 10;
+do {
+	console.log(x);
+	console.log("I AM RUNNING");
+	
+	// DONT FORGET THIS
+	x++;
+} while(x <= 8);
+
+//do..while will run 1+ times
+//while will run only if the condition applies 
