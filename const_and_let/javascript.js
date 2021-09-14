0255f8abe8d2720ea5dd8cad9a557bad9018ab92
Jabair <jabairk@gmail.com>
2021-09-14 12:02:00 -0400

    const and let

---
+++ const_and_let/javascript.js
@@ -0,0 +1,14 @@
+
+const name = "Kalob";
+
+if(name == "Kalob") {
+	// Do something
+	var/let/const brothersName = "Nathan";
+	console.log(brothersName);
+} else {
+	// Do something else
+}
+
+console.log(brothersName);
+
+
