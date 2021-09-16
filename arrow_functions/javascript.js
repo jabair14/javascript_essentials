5f993ef32436572794a62cccb1f97251140e1bfd
Jabair <jabairk@gmail.com>
2021-09-16 12:00:36 -0400

    arrow functions

---
+++ arrow_functions/javascript.js
@@ -0,0 +1,13 @@
+// Sample arrow pointer function with params
+var add = (param1, param2) => {
+	alert(param1 + param2);
+}
+
+add(13, 94);
+
+// Regular function 
+function name() {
+	name = "John";
+}
+
+console.log(name)
